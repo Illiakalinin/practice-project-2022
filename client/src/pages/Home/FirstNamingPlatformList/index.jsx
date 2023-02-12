8a78dd0c0aaf1c69683fa981dd11293ac024b1c0
illiakalinin <illinoiz4305@gmail.com>
2023-02-12 18:09:10 +0200

    Add NamingPlatform section

---
+++ client/src/pages/Home/FirstNamingPlatformList/index.jsx
@@ -0,0 +1,27 @@
+import firstNamingPlatformCards from './../FirstNamingPlatform.json';
+import styles from './FirstNamingPlatformList.module.sass';
+
+import React from 'react';
+
+function FirstNamingPlatformList () {
+  return (
+    <>
+      <div className={styles.namingPlatformListContainer}>
+        <h2>World's #1 Naming Platform</h2>
+        <ul className={styles.namingPlatformList}>
+          {firstNamingPlatformCards.map((c, i) => (
+            <li key={i}>
+              <div className={styles.namingPlatformArticle}>
+                <img src={c.iconSrc} alt={c.title} />
+                <h3>{c.title}</h3>
+                <p>{c.body}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default FirstNamingPlatformList;
